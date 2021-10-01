b29cfc77b7ece5a3f09c65a004f617691001f9b4
asas1asas200 <asas1asas200@gmail.com>
2021-10-02 06:47:26 +0800

    feat(cvector.h): add reserve method

--- cvector.h
+++ cvector.h
@@ -12,3 +12,23 @@ struct Vector{
 	ssize_t (*push_back)(struct Vector*, void*);
 	void* (*at)(struct Vector*, size_t);
 };
+
+ssize_t reserve(struct Vector* self, size_t new_capacity) {
+	void** new_array = malloc(new_capacity * sizeof(void*));
+	self->capacity = new_capacity;
+	if(!self->array) {
+		self->array = new_array;
+		return SUCCESS;
+	}
+
+	for(size_t idx = 0, max_capacity = new_capacity > self->capacity ? new_capacity : self->capacity
+		; idx < max_capacity ; ++idx) {
+		if(self->array[idx]) {
+			new_array[idx] = self->array[idx];
+		} else {
+			break;
+		}
+	}
+	free(self->array);
+	return SUCCESS;
+}
