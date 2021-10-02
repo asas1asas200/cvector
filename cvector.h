d38f6b22d9ffbe09b98bd23c53cc96f622b56300
asas1asas200 <asas1asas200@gmail.com>
2021-10-02 21:30:57 +0800

    fix(cvector.h): delete resize method
    Because I don't have any idea to add an unknown type value into array.
    If it initialzed as NULL, I must edit the pointer in underlying array outside in future,
    that means the return type of `at` method will be pointer to pointer to void,
    and it will make accessing a element value difficult.

--- cvector.h
+++ cvector.h
@@ -3,14 +3,12 @@
 #include "status_code.h"
 
 struct Vector{
-	void* begin;
-	void* end;
 	size_t capacity;
 	size_t size;
 	void** array;
 	ssize_t (*reserve)(struct Vector*, size_t);
-	void (*resize)(struct Vector*, size_t);
 	ssize_t (*push_back)(struct Vector*, void*);
+	ssize_t (*pop_back)(struct Vector*);
 	void* (*at)(struct Vector*, size_t);
 };
 
@@ -59,6 +57,6 @@ void init_vector(struct Vector** v) {
 	(*v)->push_back = push_back;
 	(*v)->at = at;
 	(*v)->size = 0;
-	(*v)->reserve(*v, 10);
+	(*v)->reserve(*v, 2);
 }
 
